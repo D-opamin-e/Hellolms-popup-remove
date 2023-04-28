398221fbb7318030301f0ac762c95b257ce4b5ff
Sang Jae Heo <66764989+D-opamin-e@users.noreply.github.com>
2023-04-29 08:43:49 +0900

    Update main.js

--- main.js
+++ main.js
@@ -9,8 +9,8 @@
 
 (function() {
 'use strict';
-let popupNotices = document.querySelectorAll('[class^="popup_notice"]');
-for(let i = 0; i < popupNotices.length; i++){
+let popupNotices = document.querySelectorAll('[class^="popup_notice"]'); //클래스 이름에 popup_notice가 들어가고 
+for(let i = 0; i < popupNotices.length; i++){ //뒤에 숫자가 붙어있는 popup_notice까지 차단 대상
 popupNotices[i].style.display = "none";
 }
 })();
